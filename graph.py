967df24444ca438b3467c8b35340290fc9f153cf
Youcheng Zhang <zhangyc.life@gmail.com>
2023-02-17 19:50:31 -0800

    use sys.maxsize & more comments

--- graph.py
+++ graph.py
@@ -16,8 +16,9 @@ ax.set_title(f.readline())
 ax.set_xlabel(f.readline())
 ax.set_ylabel(f.readline())
 
+# read data
 data = f.readline()
-min=100000000 # TODO: how to display large number in python3
+min=sys.maxsize
 while data != '':
     data, xi, time = map(float, data.split())
     speed = data / time
@@ -29,21 +30,24 @@ while data != '':
     y.append(speed / min)
     data = f.readline()
 
+# draw lines
 ax.plot(x, y, label = "Actual")
 ax.plot(x, y_ideal, label = "Ideal")
 ax.set_xticks(x)
 
+# set to log scaling
 ax.set_xscale('log', base = 2)
 ax.set_yscale('log', base = 2)
 
+# make ticker none log based
 ax.get_xaxis().set_major_formatter(matplotlib.ticker.ScalarFormatter())
 
-# label on the chart
-# for i in range(len(nodes)):
-    # plt.annotate(f"{nodes[i]:.0f}", (x[i], y[i]))
-
+# set limit for each axis
 ax.set_ylim(bottom = 1, top = x[-1]/x[0])
 ax.set_xlim(left = x[0])
 
+# display line name
 plt.legend()
+
+# save the chart
 plt.savefig(sys.argv[1].split(".")[0])
